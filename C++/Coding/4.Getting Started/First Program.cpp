391cd14cadb29c5c2d499335aaedc9cebb27067c
Shashi <shashisrivastav96@gmail.com>
2022-12-12 13:29:06 +0530

    9th

--- C++/Coding/4.Getting Started/First Program.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-int main()
-{
-    int favorite_number;
-    std::cout<<"Enter your fav no. between 1 to 100:";
-    std::cin>>favorite_number;
-    std::cout<<"Thats my fav no. too"<<std::endl;
-    return 0;
-}
